26b2be74f7367bd2d47886aa48df8eee38f35f6d
Naaman Curtis <ncurtis@kidsloop.live>
2022-06-07 09:55:30 -0600

    feat(docker): add docker image to build next.js server

--- README.md
+++ README.md
@@ -32,3 +32,17 @@ You can check out [the Next.js GitHub repository](https://github.com/vercel/next
 The easiest way to deploy your Next.js app is to use the [Vercel Platform](https://vercel.com/new?utm_medium=default-template&filter=next.js&utm_source=create-next-app&utm_campaign=create-next-app-readme) from the creators of Next.js.
 
 Check out our [Next.js deployment documentation](https://nextjs.org/docs/deployment) for more details.
+
+## Docker
+
+### Building the docker image
+
+```sh
+docker buildx build -t web-portal --build-arg NODE_AUTH_TOKEN="$NODE_AUTH_TOKEN" .
+```
+
+### Running the docker image
+
+```sh
+docker run -p 3000:3000 web-portal
+```
--- next.config.js
+++ next.config.js
@@ -1,21 +1,23 @@
 const rewrites = () => {
-    return [
-        {
-            source: "/subscriptions/:path*",
-            destination: `${process.env.SUBSCRIPTIONS_ENDPOINT}/:path*`,
-        },
-    ];
+  return [
+    {
+      source: "/subscriptions/:path*",
+      destination: `${process.env.SUBSCRIPTIONS_ENDPOINT}/:path*`,
+    },
+  ];
 };
 
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
-    images: {
-        loader: "custom" 
-    },
-    rewrites: rewrites
-    
+  reactStrictMode: true,
+  images: {
+    loader: "custom"
+  },
+  rewrites: rewrites,
+  experimental: {
+    outputStandalone: true,
+  },
 };
 
 module.exports = nextConfig;
