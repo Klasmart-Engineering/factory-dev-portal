1e3393ea9aa568762e0a3160ac70806cef997c8b
Ricardo Barata <ricardo.barata@kidsloop.live>
2022-06-08 10:52:54 +0100

    chore: expose environment variables to the browser

--- next.config.js
+++ next.config.js
@@ -1,21 +1,9 @@
-const rewrites = () => {
-    return [
-        {
-            source: "/subscriptions/:path*",
-            destination: `${process.env.SUBSCRIPTIONS_ENDPOINT}/:path*`,
-        },
-    ];
-};
-
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
     images: {
         loader: "custom" 
     },
-    rewrites: rewrites
-    
 };
 
 module.exports = nextConfig;
--- src/providers/SubscriptionsApiClientProvider.tsx
+++ src/providers/SubscriptionsApiClientProvider.tsx
@@ -10,8 +10,8 @@ export default function SubscriptionsApiClientProvider (props: Props) {
     const { children } = props;
 
     const STALE_TIME = 60 * 1000; // 60 seconds
-    const SUBSCRIPTIONS_ENDPOINT = process.env.SUBSCRIPTIONS_ENDPOINT;
-    const SUBSCRIPTIONS_REQUEST_RETRY_COUNT_MAX = +(process.env.SUBSCRIPTIONS_REQUEST_RETRY_COUNT_MAX ?? 1);
+    const SUBSCRIPTIONS_ENDPOINT = process.env.NEXT_PUBLIC_SUBSCRIPTIONS_ENDPOINT;
+    const SUBSCRIPTIONS_REQUEST_RETRY_COUNT_MAX = +(process.env.NEXT_PUBLIC_SUBSCRIPTIONS_REQUEST_RETRY_COUNT_MAX ?? 1);
 
     return (
         <KlSubscriptionsApiClientProvider
